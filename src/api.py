59701965ef1200f562d2fc1c08d199f5ed2ab9fd
NJShanta <njshanta.ca@gmail.com>
2025-04-24 01:11:53 -0400

    Add database with mockup data, add testing of mockup data, update assistant code

--- src/api.py
+++ src/api.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
+import sqlite3
+from datetime import datetime
 
 app = FastAPI()
 
@@ -28,10 +30,23 @@ class AudioFeatures(BaseModel):
 def predict_clinical(data: ClinicalFeatures):
     features = np.array([[data.age, data.mmse, data.depression]])
     pred = clinical_model.predict(features)[0]
+    log_prediction("clinical", [data.age, data.mmse, data.depression, None, None, None, None], int(pred))
     return {"clinical_risk_prediction": int(pred)}
 
 @app.post("/predict-audio-risk")
 def predict_audio(data: AudioFeatures):
     features = np.array([[data.duration, data.tempo, data.zero_crossing_rate, data.rms_energy]])
     pred = audio_model.predict(features)[0]
+    log_prediction("audio", [None, None, None, data.duration, data.tempo, data.zero_crossing_rate, data.rms_energy], int(pred))
     return {"audio_risk_prediction": int(pred)}
+
+def log_prediction(input_type, values, prediction):
+    conn = sqlite3.connect("dementia_predictions.db")
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO dementia_predictions 
+        (input_type, age, mmse, depression, duration, tempo, zero_crossing_rate, rms_energy, prediction)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (input_type, *values, prediction))
+    conn.commit()
+    conn.close()
